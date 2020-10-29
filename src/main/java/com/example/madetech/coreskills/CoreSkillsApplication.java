b564f91785b100151d497a1e1eb0205807771762
MadeTech Dushan <dushan@madetech.com>
2020-10-29 16:04:56 +0000

    Initial commit

---
+++ src/main/java/com/example/madetech/coreskills/CoreSkillsApplication.java
@@ -0,0 +1,13 @@
+package com.example.madetech.coreskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSkillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/madetech/coreskills/CoreSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madetech.coreskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
