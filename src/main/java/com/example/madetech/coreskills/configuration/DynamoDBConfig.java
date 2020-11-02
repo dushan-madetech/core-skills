f365fe932b0c7b6a9a9b1cd659ead053eabd358f
csabagyorfi <csaba@madetech.com>
2020-11-02 15:34:13 +0000

    WIP

--- src/main/java/com/example/madetech/coreskills/configuration/DynamoDBConfig.java
+++ src/main/java/com/example/madetech/coreskills/configuration/DynamoDBConfig.java
@@ -1,8 +1,9 @@
 package com.example.madetech.coreskills.configuration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,6 @@ import org.springframework.util.StringUtils;
 @EnableDynamoDBRepositories
         (basePackages = "com.example.madetech.coreskills.repositories")
 public class DynamoDBConfig {
-
     @Value("${amazon.dynamodb.endpoint}")
     private String amazonDynamoDBEndpoint;
 
@@ -23,21 +23,23 @@ public class DynamoDBConfig {
     @Value("${amazon.aws.secretkey}")
     private String amazonAWSSecretKey;
 
+    @Value("${amazon.aws.region}")
+    private String awsRegion;
+
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        AmazonDynamoDB amazonDynamoDB
-                = new AmazonDynamoDBClient(amazonAWSCredentials());
+        AmazonDynamoDB amazonDynamoDB;
 
-        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
-            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint) && !StringUtils.isEmpty(awsRegion)) {
+            amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+                    new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, awsRegion))
+                    .build();
+        } else {
+            amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+                    new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "eu-west-2"))
+                    .build();
         }
 
         return amazonDynamoDB;
-    }
-
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(
-                amazonAWSAccessKey, amazonAWSSecretKey);
     }
 }
